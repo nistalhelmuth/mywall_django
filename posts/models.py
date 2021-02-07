bd25b02cb41946caa071992bb95faa2abd5c51bb
uvg <nis16104@uvg.edu.gt>
2021-02-07 09:08:07 -0600

    post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Comment(models.Model):
+    content = models.CharField(max_length = 280)
+    created_by = models.ForeignKey(User, on_delete = models.DO_NOTHING) # check
+    date_created = models.DateTimeField(default = timezone.now)
+    date_modified = models.DateTimeField(null = True)
+
+    def __str__(self):
+        return ('%s-%s-%s') % self.created_by, self.date_created
+
+
+class Post(models.Model):
+    content = models.CharField(max_length = 280)
+    # photo = models.CharField()
+    created_by = models.ForeignKey(User, on_delete = models.DO_NOTHING) # check
+    date_created = models.DateTimeField(default = timezone.now)
+    date_modified = models.DateTimeField(null = True)
+    comments = models.ManyToManyField(Comment, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return ('%s-%s-%s') % self.created_by, self.date_created
+
+    
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post, Comment
+from rest_framework import viewsets
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
