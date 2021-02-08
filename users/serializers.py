9eaa12b128c6da39601be0cbabd7beaae26084fa
uvg <nis16104@uvg.edu.gt>
2021-02-08 15:24:25 -0600

    add profile info to login

--- users/serializers.py
+++ users/serializers.py
@@ -27,3 +27,24 @@ class UserSerializer(serializers.ModelSerializer):
         repr_dict = super(UserSerializer, self).to_representation(value)
         return OrderedDict((k, v) for k, v in repr_dict.items() 
                            if v not in [None, [], '', {}])
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    date_created = serializers.ReadOnlyField()
+
+    class Meta(object):
+        model = User
+        fields = (
+            'id',
+            'email',
+            'name',
+            'city',
+            'visitors',
+            'genre',
+            'feeling',
+            'date_created',
+        )
+    
+    def to_representation(self, value):
+        repr_dict = super(UserDetailSerializer, self).to_representation(value)
+        return OrderedDict((k, v) for k, v in repr_dict.items() 
+                           if v not in [None, [], '', {}])
--- users/views.py
+++ users/views.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.views import APIView
 from rest_framework.generics import RetrieveAPIView
 from rest_framework_jwt.utils import jwt_payload_handler
 import jwt
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserDetailSerializer
 from .models import User
 
 @api_view(['POST'])
@@ -28,33 +28,27 @@ def authenticate_user(request):
         email = request.data['email']
         password = request.data['password']
         user = User.objects.get(email=email, password=password)
-        try:
-            payload = jwt_payload_handler(user)
-            token = jwt.encode(payload, settings.SECRET_KEY)
-            user_details = {
-                'name': user.name,
-                'token': token,
-            }
-            user_logged_in.send(
-                sender=user.__class__,
-                request=request,
-                user=user
-            )
-            return Response(user_details, status=status.HTTP_200_OK)
+        payload = jwt_payload_handler(user)
+        token = jwt.encode(payload, settings.SECRET_KEY)
+        serializer = UserDetailSerializer(user)
+        new_serializer = {'token': token}
+        new_serializer.update(serializer.data)
+        user_logged_in.send(
+            sender=user.__class__,
+            request=request,
+            user=user
+        )
+        return Response(new_serializer, status=status.HTTP_200_OK)
 
-        except Exception as e:
-            res = {'error': 'something went wrong :('}
-            return Response(res, status=status.HTTP_403_FORBIDDEN)
     except ObjectDoesNotExist:
         res = {'error': 'Check the credentials'}
         return Response(res, status=status.HTTP_403_FORBIDDEN)
  
     
 class UserProfileAPIView(RetrieveAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = UserDetailSerializer
  
     def get(self, request, *args, **kwargs):
-        serializer = self.serializer_class(request.user)
- 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return self.retrieve(request, *args, **kwargs)
