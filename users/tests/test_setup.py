257afb47e344b542cf179c59e0063ff65073db1b
uvg <nis16104@uvg.edu.gt>
2021-02-12 19:11:07 -0600

    users testing

---
+++ users/tests/test_setup.py
@@ -0,0 +1,22 @@
+from rest_framework.test import APITestCase
+from faker import Faker
+
+from users.models import User
+
+class TestSetUp(APITestCase):
+
+    def setUp(self):
+        self.fake = Faker()
+
+        self.user_data = {
+            'email': self.fake.email(),
+            'name': self.fake.name(),
+            "city": "Test City",
+            "genre": "M",
+            'password': self.fake.email(),
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
---
+++ users/tests/test_view.py
@@ -0,0 +1,28 @@
+import json
+from rest_framework import status
+from .test_setup import TestSetUp
+from users.models import User
+
+class UsersTestCase(TestSetUp):
+
+    def test_registration(self):
+        response = self.client.post("/users/register/", self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["email"], self.user_data["email"])
+
+    def test_retrieve_profile(self):
+        self.client.post("/users/register/", self.user_data)
+        response = self.client.get("/users/profile/3/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_authentication(self):
+        self.client.post("/users/register/", self.user_data)
+        response = self.client.post(
+            "/users/login/", 
+            json.dumps({
+                'email': self.user_data["email"],
+                "password": self.user_data["password"]
+            }), 
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+   
