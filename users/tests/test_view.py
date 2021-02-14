6efebc585495cbdacd84cd0dc54ec11ffdc8f563
uvg <nis16104@uvg.edu.gt>
2021-02-13 22:04:13 -0600

    models testing

--- users/tests/test_view.py
+++ users/tests/test_view.py
@@ -11,8 +11,8 @@ class UsersTestCase(TestSetUp):
         self.assertEqual(response.data["email"], self.user_data["email"])
 
     def test_retrieve_profile(self):
-        self.client.post("/users/register/", self.user_data)
-        response = self.client.get("/users/profile/3/")
+        response = self.client.post("/users/register/", self.user_data)
+        response = self.client.get("/users/profile/%d/" % (response.data["id"]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_authentication(self):
